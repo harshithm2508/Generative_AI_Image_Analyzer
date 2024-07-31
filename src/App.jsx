db324c362eb969b78fbaf6f46ed3781f1242af73
harshithm2508 <harshithm2508@gmail.com>
2024-07-31 15:16:39 +0530

    note for prompt-less conversation

--- src/App.jsx
+++ src/App.jsx
@@ -75,9 +75,12 @@ export default function App() {
         onChange={handleFileChange}
       />
 
-      <div className=" h-12 w-1/2 border-2 border-blue-400 rounded-md flex overflow-hidden">
-                <input type="text" className="w-full focus:outline-none p-2" onChange={(e)=>{setPromptInput(e.target.value)}}/>
-                <button onClick={run} disabled={loading} className=' bg-lime-400 px-4 text-slate-700 font-semibold'>{loading ? 'Processing....' : "Ask"}</button>
+      <div className=' w-1/2'>
+        <div className=" h-12 border-2 border-blue-400 rounded-md flex overflow-hidden">
+                  <input type="text" className="w-full focus:outline-none p-2" onChange={(e)=>{setPromptInput(e.target.value)}}/>
+                  <button onClick={run} disabled={loading} className=' bg-lime-400 px-4 text-slate-700 font-semibold'>{loading ? 'Processing....' : "Ask"}</button>
+        </div>
+        <p className=' text-white'>**  If you do not provide any prompt, information or comparison of the images will be displayed.</p>
       </div>
 
       <div className=' h-2/4 w-1/2 bg-white p-2 rounded-md'>
